144d2214a52f92260a529d8813c0feee7840fe93
David Lynch <kemayo@gmail.com>
2018-07-21 21:34:45 -0500

    Wrong tooltip on world map

--- Directions.lua
+++ Directions.lua
@@ -54,7 +54,7 @@ local clickedLandmarkZone = nil
 local lastGossip = nil
 
 function HDHandler:OnEnter(mapID, coord)
-	local tooltip = self:GetParent() == WorldMapButton and WorldMapTooltip or GameTooltip
+	local tooltip = self:GetParent() == WorldMapFrame:GetCanvas() and WorldMapTooltip or GameTooltip
 	if ( self:GetCenter() > UIParent:GetCenter() ) then -- compare X coordinate
 		tooltip:SetOwner(self, "ANCHOR_LEFT")
 	else
@@ -139,7 +139,7 @@ function HDHandler:OnClick(button, down, mapID, coord)
 end
 
 function HDHandler:OnLeave(mapFile, coord)
-	if self:GetParent() == WorldMapButton then
+	if self:GetParent() == WorldMapFrame:GetCanvas() then
 		WorldMapTooltip:Hide()
 	else
 		GameTooltip:Hide()
